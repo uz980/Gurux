abadb6b6b7bc724b6d8298411dda3b909eac38e5
uz980 <shaxriyordoliyev4@gmail.com>
2026-01-27 19:52:30 +0500

    update akamuchun1.py

--- akamuchun1.py
+++ akamuchun1.py
@@ -63,7 +63,7 @@ else:
 signature = hashlib.sha256((device_id + SECRET_TOKEN).encode()).hexdigest()
 
 # Serverga yuboramiz
-url = "https://68f77a7f47cf9.myxvest1.ru/Termuxguruxkolar/Akamga/secure_api.php"
+url = "https://68f77a7f47cf9.myxvest1.ru/Termuxguruxkolar/Akamga1/secure_api.php"
 params = {
     "api": API_KEY,
     "id": device_id,
